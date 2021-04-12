0b5cf34035c23ab13288fa5be86672d52ee4632d
yixianlv <yixian.lyu@adlinktech.com>
2021-04-12 10:19:18 +0200

    watch changes
    Signed-off-by: yixianlv <yixian.lyu@adlinktech.com>

--- ddsls/ddsls.py
+++ ddsls/ddsls.py
@@ -8,6 +8,7 @@ from cyclonedds.domain import DomainParticipant
 from cyclonedds.builtin import (BuiltinDataReader, BuiltinTopicDcpsParticipant,
                                 BuiltinTopicDcpsSubscription,  BuiltinTopicDcpsPublication)
 from cyclonedds.util import duration
+from cyclonedds.core import WaitSet, ReadCondition, ViewState, InstanceState, SampleState
 
 
 def create_parser():
@@ -22,18 +23,44 @@ def create_parser():
     return args
 
 
-def manage_dcps_object(dp, topic_type, topic):
-    dr = BuiltinDataReader(dp, topic)
-    samples = dr.take_iter(timeout=duration(milliseconds=10))
+def print_object(fp, obj, print_json):
+    if print_json:
+        json.dump(obj, fp, indent=4)
+    else:
+        fp.write(str(obj) + "\n")
+        fp.flush()
 
+
+result = []
+
+
+def open_file(filename, obj, print_json):
+    if not filename:
+        print_object(sys.stdout, obj, print_json)
+    else:
+        result.append(obj)
+        with open(filename, mode="w") as fp:
+            try:
+                print_object(fp, result, print_json)
+                print("Write to file " + filename)
+                fp.close()
+                return 0
+            except Exception:
+                return 1
+
+
+def manage_samples(dr, samples, topic_type, filename, print_json):
+    obj = []
     for sample in samples:
-        if topic_type == 'PARTICIPANT':
+        if topic_type == "PARTICIPANT":
             sample = {
                 topic_type: [{
                     "key": str(sample.key)
                 }]
             }
-        else:
+            obj.append(sample)
+        elif ((topic_type == "SUBSCRIPTION" and sample.key == dr.get_guid()) or
+              topic_type == "PUBLICATION"):
             sample = {
                 topic_type: [{
                     "key": str(sample.key),
@@ -42,20 +69,33 @@ def manage_dcps_object(dp, topic_type, topic):
                     "qos": sample.qos.asdict()
                 }]
             }
-        return sample
+            obj.append(sample)
+    if len(obj):
+        open_file(filename, obj, print_json)
 
 
-def print_object(fp, obj, print_json):
-    if print_json:
-        json.dump(obj, fp, indent=4)
+def manage_dcps_object(dp, dr, topic_type, filename, print_json):
+    waitset = WaitSet(dp)
+    condition = ReadCondition(dr, ViewState.Any | InstanceState.Alive | SampleState.NotRead)
+    disposed_cond = ReadCondition(dr, ViewState.Any | InstanceState.NotAliveDisposed | SampleState.Any)
+    waitset.attach(condition)
+
+    samples = dr.take(condition=condition)
+    disposed_samples = dr.take(condition=disposed_cond)
+
+    if len(samples) or len(disposed_samples):
+        if len(samples):
+            manage_samples(dr, samples, topic_type, filename, print_json)
+        else:
+            print("\n--- " + topic_type + " disposed ---")
+            manage_samples(dr, disposed_samples, topic_type, filename, print_json)
     else:
-        fp.write(str(obj))
+        waitset.wait(duration(milliseconds=20))
 
 
 def main():
     args = create_parser()
     dp = DomainParticipant(args.id)
-    obj = []
 
     if args.topics:
         if args.topics == "dcpsparticipant":
@@ -67,24 +107,17 @@ def main():
         else:
             type = "PUBLICATION"
             topic = BuiltinTopicDcpsPublication
-        obj.append(manage_dcps_object(dp, type, topic))
+        dr = BuiltinDataReader(dp, topic)
+        manage_dcps_object(dp, dr, type, args.filename, args.json)
 
     if args.a is True:
         type = ["PARTICIPANT", "SUBSCRIPTION", "PUBLICATION"]
-        topic = [BuiltinTopicDcpsParticipant, BuiltinTopicDcpsSubscription, BuiltinTopicDcpsPublication]
-        for i in range(len(type)):
-            obj.append(manage_dcps_object(dp, type[i], topic[i]))
-
-    if not args.filename:
-        print_object(sys.stdout, obj, args.json)
-    else:
-        with open(args.filename, mode="w") as fp:
-            try:
-                print_object(fp, obj, args.json)
-                fp.close()
-                return 0
-            except Exception:
-                return 1
+        dr = [BuiltinDataReader(dp, BuiltinTopicDcpsParticipant),
+              BuiltinDataReader(dp, BuiltinTopicDcpsSubscription),
+              BuiltinDataReader(dp, BuiltinTopicDcpsPublication)]
+        while True:
+            for i in range(len(type)):
+                manage_dcps_object(dp, dr[i], type[i], args.filename, args.json)
 
 
 if __name__ == '__main__':
