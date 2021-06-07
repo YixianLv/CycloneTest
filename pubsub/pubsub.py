4432c250f422e7dfbb508b1d33ad53cc3be3c9b1
yixianlv <yixian.lyu@adlinktech.com>
2021-06-07 15:03:25 +0200

    trouble with syntax '('
    Signed-off-by: yixianlv <yixian.lyu@adlinktech.com>

--- pubsub/pubsub.py
+++ pubsub/pubsub.py
@@ -2,15 +2,17 @@
 import sys
 import select
 import argparse
+import re
+from dataclasses import fields
 from datastruct import Integer, String
+from util import qos_help
 from cyclonedds.core import WaitSet, ReadCondition, ViewState, InstanceState, SampleState
 from cyclonedds.domain import DomainParticipant
 from cyclonedds.pub import DataWriter
 from cyclonedds.sub import DataReader
 from cyclonedds.topic import Topic
 from cyclonedds.util import duration
-from cyclonedds.qos import Qos
-from dataclasses import fields
+from cyclonedds.qos import Qos, Policy
 
 
 class Topicmanger:
@@ -43,41 +45,68 @@ class Topicmanger:
             # print("Subscribed: {seq =", sample.seq, ", keyval =", sample.keyval, "}")
 
 
-def qos_fields(policies, args):
-    qos = 0
-    if len(args.qos) == 1:
-        qos = Qos(policies[args.qos[0]])
-    elif len(args.qos) > 1:
+class Qosmanager(Topicmanger):
+    def __init__(self):
+        self.parse_qos = re.compile(r"^([\w\d\-\.]+)(\([\w\d\.\-]+(?:,[\w\d\.\-]+)*\))?$")
+
+    def construct_policy(self, txt):
+        m = self.parse_qos.match(txt)
+        if not m:
+            raise Exception(f"Invalid format for qos '{txt}'")
+
+        policy_name = m.group(1)
+
+        if not policy_name.startswith("Policy."):
+            policy_name = f"Policy.{policy_name}"
+
+        if not policy_name in Qos._policy_mapper:
+            raise Exception(f"Unknown qos policy {policy_name}")
+
+        policy = Qos._policy_mapper[policy_name]
+        _fields = fields(policy)
+
+        if len(_fields) == 0 and m.group(2) is None:
+            # No arguments needed and none provided
+            return policy
+        elif len(_fields) == 0:
+            # No arguments needed but are provided
+            raise Exception(f"{policy_name} requires no arguments but some were provided.")
+        elif m.group(2) is None:
+            # Arguments needed but none provided
+            raise Exception(f"{policy_name} requires arguments but none were provided.")
+
+        # Trim of outer parenthesis and split
+        args = m.group(2).strip("()").split(',')
+
+        if len(args) != len(_fields):
+            raise Exception(f"{policy_name} requires {len(_fields)} arguments but {len(args)} were provided.")
+
+        # convert to correct types
         try:
-            if len(args.qos) - 1 != len(fields(policies[args.qos[0]])):
-                print(f"Error! The numbers of variables needed for the qos is {len(fields(policies[args.qos[0]]))}, "
-                      f"but {len(args.qos) - 1} variables was inputted.")
-                for field in fields(policies[args.qos[0]]):
-                    print(f"  {field.name} {field.type}")
-                sys.exit(1)
-            else:
-                qos = Qos(policies[args.qos[0]](args.qos[1:]))
-                print(args.qos[1:])
-
-        except KeyError:
-            print("Qos incorrect: Select a Qos from the Qos list, eg.Policy.History.KeepAll, Policy.History.KeepLast 10")
-            sys.exit(1)
-    return qos
+            args = [field.type(arg) for field, arg in zip(_fields, args)]
+        except ValueError as e:
+            raise Exception(f"Incorrect type provided for an argument, {e}")
+        return policy(*args)
 
 
-def create_parser(policies):
-    parser = argparse.ArgumentParser()
+def create_parser():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-T", "--topic", type=str, required=True, help="The name of the topic to publish/subscribe to")
-    parser.add_argument("-q", "--qos", nargs="*", help=f'''Customize the qos for the entities, eg.Policy.History.KeepAll, Policy.History.KeepLast 10\n
-                        Available qos are: {policies.keys()}''')
+    parser.add_argument("-q", "--qos", help="Set QoS for entities, check '--qoshelp' for available QoS and usage")
+    parser.add_argument("--qoshelp", action="store_true", help="Avaialbe QoS and usage are:\n" + "\n".join(map(str, qos_help())))
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
     args = parser.parse_args()
     return args
 
 
 def main():
     policies = Qos._policy_mapper
-    args = create_parser(policies)
-    qos = qos_fields(policies, args)
+    qos_manager = Qosmanager()
+    qos = None
+    args = create_parser()
+    if args.qos:
+        qos = qos_manager.construct_policy(args.qos)
 
     dp = DomainParticipant(0)
     waitset = WaitSet(dp)
